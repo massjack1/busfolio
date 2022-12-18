28811ff3d96d78dc98a01ba7e66e2fcda9af5ffc
Mass Jack <77391015+massjack1@users.noreply.github.com>
2022-12-18 17:03:26 +0100

    Remove motion fade styles

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,32 +8,21 @@ import {motion} from "framer-motion";
 
 const AboutSection = () => {
 
-  const titleAnim = {
-    hidden: {opacity: 0},
-    show: {opacity: 1, transition: {duration:2 }},
-  };
-  const container = {
-    hidden: { x: 100},
-    show: { x: 0, transition: { duration: 0.75, staggerChildren: 1, when:"afterChildren"}},
-  };
-
   return (
     <About>
       <Description>
-        <motion.div variants={container} initial="hidden" animate="show" className="title">
+        <motion.div>
           <Hide>
             <motion.h2 
-            variants={titleAnim} >We work to make</motion.h2>
+            >We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 
-            variants={titleAnim}>
+            <motion.h2>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 
-            variants={titleAnim}>true.</motion.h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
         </motion.div>
         <p>
